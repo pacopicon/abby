5ee47488df08e308a442c61ce6db34214787330a
pacopicon <palmtreerooskee@gmail.com>
2017-03-27 06:18:13 -0400

    creating tic-tac-toe game

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Game from './Game';
+// import Square from './Square';
 import './index.css';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
+);
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('container')
 );
